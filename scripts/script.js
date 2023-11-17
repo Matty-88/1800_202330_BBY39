54bb3347d3746496ecf6c8bd96bd9d88430d618f
amal allaham <amalallahham20@gmail.com>
2023-11-16 16:54:07 -0800

    goal

--- scripts/script.js
+++ scripts/script.js
@@ -15,12 +15,19 @@ function logout() {
 }
 
 function getNameFromAuth() {
-  firebase.auth().onAuthStateChanged((user) => {
+  firebase.auth().onAuthStateChanged(async (user) => {
     if (user) {
       // User is signed in, see docs for a list of available properties
       // https://firebase.google.com/docs/reference/js/v8/firebase.User
       var uid = user.uid;
+      console.log("uid", uid);
+      const querySnapshot = await db
+        .collection("goals")
+        .where("userIds", "array-contains", uid).get()
 
+      if (querySnapshot.size > 0) {
+        console.log(querySnapshot.docs());
+      }
       // ...
     } else {
       // User is signed out
@@ -28,6 +35,5 @@ function getNameFromAuth() {
     }
   });
 }
-
 
 getNameFromAuth();
