f9b804fa172c6330ec4200e886478a6be0477468
Inder2003  <inderjitdhillon1112@gmail.com>
2023-11-16 16:18:38 -0800

    added a post to test

--- scripts/main.js
+++ scripts/main.js
@@ -84,3 +84,17 @@ function displayCardsDynamically(collection) {
 }
 
 displayCardsDynamically("hikes");  //input param is the name of the collection
+var ImageFile;
+function listenFileSelect() {
+      // listen for file selection
+      var fileInput = document.getElementById("mypic-input"); // pointer #1
+      const image = document.getElementById("mypic-goes-here"); // pointer #2
+
+			// When a change happens to the File Chooser Input
+      fileInput.addEventListener('change', function (e) {
+          ImageFile = e.target.files[0];   //Global variable
+          var blob = URL.createObjectURL(ImageFile);
+          image.src = blob; // Display this image
+      })
+}
+listenFileSelect();
